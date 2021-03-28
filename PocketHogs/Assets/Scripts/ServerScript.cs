ea148c9c5ea0e937c747cc7bc921d61a53cf9c2c
eliana perez <eliana.perez@mymail.champlain>
2021-03-28 12:15:27 -0400

    got a base networking tutorial working so now we can actually make game

---
+++ PocketHogs/Assets/Scripts/ServerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.Networking;
+public class ServerScript : MonoBehaviour 
+{
+    private const int MAX_CONNECTION = 6;
+    private int port = 5701;
+
+    private int hostID;
+    private int webHostId;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private bool isStarted = false;
+    private byte error;
+
+    private void Start()
+    {
+        NetworkTransport.Init();
+        ConnectionConfig cc = new ConnectionConfig();
+
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+        unreliableChannel = cc.AddChannel(QosType.Unreliable);
+
+        HostTopology topop = new HostTopology(cc, MAX_CONNECTION);
+
+        hostID = NetworkTransport.AddHost(topop, port, null);
+        webHostId = NetworkTransport.AddWebsocketHost(topop, port, null);
+
+        isStarted = true;
+    }
+
+    private void Update()
+    {
+        
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing: break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("Player " + connectionId + " has connected");
+                break;
+            case NetworkEventType.DataEvent:
+                string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                Debug.Log("Player " + connectionId + " has sent: ");
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("Player " + connectionId + " has disconnected");
+                break;
+
+            case NetworkEventType.BroadcastEvent:
+
+                break;
+        }
+    }
+}
