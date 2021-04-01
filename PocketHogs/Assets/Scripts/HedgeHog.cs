f99795fee7fbca5e42953cff66598cf41d097195
Eliana <eliana.perez@mymail.champlain.edu>
2021-04-01 18:29:38 -0400

    wandering script works!

--- PocketHogs/Assets/Scripts/HedgeHog.cs
+++ PocketHogs/Assets/Scripts/HedgeHog.cs
@@ -3,18 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HedgeHog : MonoBehaviour {
+	//variables
+	public enum States
+	{
+		Wandering,
+		SeekFood,
+		EatFood,
+		Fleeing
+	};
 
+	private States currentState;
+	private Wandering wandering;
+	//functions
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		currentState = States.Wandering;
+		wandering = GetComponent<Wandering>();
+
+		//right now this is in here but WE HAVE TO MOVE IT LATER (basically other components will call it)
+		ChangeState(States.Wandering);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 	public void Destroy()
 	{
 		GameObject.Destroy(this);
+	}
+
+	public void ChangeState(States newState)
+	{
+		currentState = newState;
+		switch (currentState)
+		{
+			case States.Wandering:
+				wandering.ChangeIfWandering(true);
+				break;
+			case States.SeekFood:
+				//put seekfood here
+				break;
+			case States.Fleeing:
+				//put fleeing here
+				break;
+			case States.EatFood:
+				//put eatfood here
+				break;
+			default:
+				wandering.ChangeIfWandering(true);
+				break;
+		}
 	}
 }
---
+++ PocketHogs/Assets/Scripts/Wandering.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wandering : MonoBehaviour {
+	private Vector3 targetLocation;
+	public Vector2 spawnMin;
+	public Vector2 spawnMax;
+	public float speed = 5f;
+	private bool areWandering = false;
+	private Rigidbody2D rb;
+
+	//tutorial stuff (delete if no worky)
+	private bool isMoving;
+	private Vector3 currentPos, gridTarget;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		ChangeTarget();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (areWandering)
+		{
+			Vector3 diff = targetLocation - transform.position;
+
+
+			//velocity stuff
+			if (diff.magnitude < 1)
+			{
+				ChangeTarget();
+			}
+			diff = new Vector2(diff.normalized.x, diff.normalized.y);
+			diff *= speed;
+			rb.velocity = diff;
+			
+
+
+		}
+		
+	}
+
+	public void ChangeIfWandering(bool x)
+    {
+		areWandering = x;
+    }
+	private void ChangeTarget()
+	{
+		targetLocation = new Vector3(Random.Range(spawnMin.x, spawnMax.x), Random.Range(spawnMin.y, spawnMax.y), 0);
+	}
+
+	//if they collide with something they change direction
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		ChangeTarget();
+	}
+}
