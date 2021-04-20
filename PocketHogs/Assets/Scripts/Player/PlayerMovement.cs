831baa1ab53d6f15e9edcb54d74c6a685f098ff5
Theo Schnorrenberg <theo.schnorrenberg@mymail.champlain.edu>
2021-04-20 13:47:26 -0400

    Player movement (local) added

---
+++ PocketHogs/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	float xMove;
+	float yMove;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		CheckInput();
+		UpdatePos();
+	}
+
+	private void CheckInput()
+	{
+		float xMove = Input.GetAxis("Horizontal");
+		float yMove = Input.GetAxis("Vertical");
+	}
+
+	private void UpdatePos()
+	{
+		transform.Translate(xMove, yMove, 0);
+
+		// Use unreliable, update with unit position
+	}
+}
